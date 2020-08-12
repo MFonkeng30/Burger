4aa7c467824100be4c6abdd2d8571ea28e0e747d
Muendem Fonkeng <fonkengmuendem@yahoo.com>
2020-08-12 00:20:49 -0500

    syntax changes

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ USE burgers_db;
 
 CREATE TABLE burgers (
     id INT AUTO_INCREMENT NOT NULL,
-    burger_name STRING NULL,
+    burger_name VARCHAR(30) NOT NULL,
     devoured BOOLEAN,
     PRIMARY KEY (id)
 )
