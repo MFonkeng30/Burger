466c61b5807d6f7d4db66252d0240afb5f1729c5
Muendem Fonkeng <fonkengmuendem@yahoo.com>
2020-08-13 00:56:19 -0500

    code that calls orm functions

--- models/burger.js
+++ models/burger.js
@@ -1,9 +1,25 @@
-var orm = require("../config/orm.js");
+const orm = require("../config/orm.js");
 
 //Code to call for orm functions
-
-
-
-
+// Select ALL 
+const burger = {
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
+    },
+    // Insert One
+    insertOne: function (column, value, cb) {
+        orm.insertOne("burgers", column, value, function (res) {
+            cb(res);
+        })
+    },
+    //Update One 
+    updateOne: function (id, cb) {
+        orm.updateOne("burgers", "devoured", true, id, function (res) {
+            cb(res);
+        })
+    }
+};
 
 module.exports = burger;
