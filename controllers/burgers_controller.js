d1bfcb3b93eb2e4d7faefe68d7e4be4fa5b37748
Muendem Fonkeng <fonkengmuendem@yahoo.com>
2020-08-13 01:05:11 -0500

    required express.router and created functions

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,10 +1,40 @@
-var express = require("express");
-var burger = require("../models/burger.js");
-const { Router } = require("express");
-
+const express = require("express");
+const router = express.Router();
+const burger = require("../models/burger.js");
 
 //Router function 
+// GET
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
 
+        var hbsObject = {
+            burgers: data
+        };
 
+        res.render("index", hbsObject);
+    });
+});
 
-module.exports = Router
+// POST 
+router.post("/api/burgers", function (req, res) {
+    const burgerName = req.body.burger_name;
+    burger.insertOne("burger_name", burgerName, function (result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+// PUT 
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.updateOne(req.params.id, function (result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
