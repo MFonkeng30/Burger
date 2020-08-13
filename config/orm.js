09410777c1f3cb1c593e61277cbb9379ab070958
Muendem Fonkeng <fonkengmuendem@yahoo.com>
2020-08-13 00:18:55 -0500

    created necessary functions

--- config/orm.js
+++ config/orm.js
@@ -1,17 +1,31 @@
-var connection = require("../config/connection.js");
-
-//Helper function to convert into SQL Syntax
-
-
-
-
+const connection = require("../config/connection.js");
 
 //Object for all SQL functions
-
-
-
-
-
-
+const orm = {
+    // Action SelectAll
+    selectAll: function (tableName, cb) {
+        const queryString = " SELECT * FROM " + tableName + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            cb(result);
+        })
+    },
+    // Action InsertOne 
+    insertOne: function (tableName, column, value, cb) {
+        const queryString = `INSERT INTO ${tableName} (${column}, devoured) VALUES ("${value}", 0);`
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            cb(result)
+        });
+    },
+    // Action UpdateOne
+    updateOne: function (tableName, column, condition, value, cb) {
+        const queryString = `UPDATE ${tableName} SET ${column}=${condition} WHERE id=${value};`;
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            cb(result);
+        })
+    }
+};
 
 module.exports = orm;
