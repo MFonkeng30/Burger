cecab3cafd8ecc5dca736aa25fc3d8ef80b71e41
Muendem Fonkeng <fonkengmuendem@yahoo.com>
2020-08-22 09:54:06 -0500

    syntax changes

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
-var mysql = require("mysql")
+const mysql = require("mysql")
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "BlackRoses30@",
+    password: "",
     database: "burgers_db"
 });
 
